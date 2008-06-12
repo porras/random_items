30394e5ab514a9d44f71cbd50b878b06d53a88d6
Sergio Gil <sgilperez@gmail.com>
2008-06-12 16:27:08 +0200

    Real code =;-)

--- lib/random_items.rb
+++ lib/random_items.rb
@@ -1,1 +1,37 @@
-# RandomItems
+class ActiveRecord::Base
+  
+  # More or less the same interface as ActiveRecord::Base.find
+  # Some examples:
+  #   Model.random(:first)                          # picks a random item
+  #   Model.random(:all, :limit => 5)               # picks 5 random items
+  #   Model.random(:first, :include => :relation)   # picks a random item using standard
+  #                                                 # AR::B.find options, such as :include
+  # In that last case, you are expected not to use options which don't make sense, such as :order =;-)
+  #
+  def self.random(items, options = {})
+    if items == :first
+      random_first(options)
+    elsif items == :all
+      random_all(options)
+    else
+      raise ArgumentError
+    end
+  end
+  
+  private
+  
+  def self.random_all(options = {})
+    options[:limit] ||= 1
+    limit = options.delete(:limit)
+    (1..limit.to_i).to_a.map { random_first(options) }
+  end
+  
+  def self.random_first(options = {})
+    case c = self.count
+    when 0: nil
+    when 1: find(:first)
+    else    find(:first, options.merge(:limit => 1, :offset => rand(c - 1)))
+    end
+  end
+  
+end
--- test/random_items_test.rb
+++ test/random_items_test.rb
@@ -1,8 +1,64 @@
 require 'test/unit'
+require 'rubygems'
+require 'active_record'
+require 'mocha'
+require 'shoulda'
+require 'random_items'
+
+class Model < ActiveRecord::Base;end
 
 class RandomItemsTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  
+  context "random(:first)" do
+    should "find a random item" do
+      Model.expects(:count).returns(10)
+      Model.expects(:rand).with(9).returns(1)
+      Model.expects(:find).with(:first, { :limit => 1, :offset => 1 })
+      Model.random(:first)      
+    end
+    context "in a table with one item" do
+      should "find that item" do
+        Model.expects(:count).returns(1)
+        Model.expects(:rand).never
+        Model.expects(:find).with(:first)
+        Model.random(:first)        
+      end
+    end
+    context "in a table with zero items" do
+      should "return nil" do
+        Model.expects(:count).returns(0)
+        Model.expects(:rand).never
+        Model.expects(:find).never
+        assert_nil Model.random(:first)  
+      end
+    end
+    context "with options" do
+      should "forward them to find()" do
+        Model.expects(:count).returns(10)
+        Model.expects(:rand).with(9).returns(1)
+        Model.expects(:find).with(:first, { :limit => 1, :offset => 1, :include => :other_model })
+        Model.random(:first, :include => :other_model)        
+      end
+    end
   end
+  
+  context "random(:all)" do
+    should "find :limit random items" do
+      Model.expects(:random_first).times(3)
+      Model.random(:all, :limit => 3)      
+    end
+    context "with no :limit" do
+      should "default to 1" do
+        Model.expects(:random_first)
+        Model.random(:all)        
+      end
+    end
+    context "with options" do
+      should "forward them to random_first" do
+        Model.expects(:random_first).times(3).with(:include => :other_model)
+        Model.random(:all, :limit => 3, :include => :other_model)
+      end
+    end
+  end
+  
 end
